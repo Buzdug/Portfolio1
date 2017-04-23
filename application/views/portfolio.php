c4b3d442ee056986c032831090b31e4575effb5b
Buzdug <adrianbuzdug@gmail.com>
2017-04-23 23:03:07 +0300

    Commit all

---
+++ application/views/portfolio.php
@@ -0,0 +1,138 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Designs by Adrian Buzdug</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap Core CSS -->
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Palanquin:300,600" rel="stylesheet">
+	<link href="css/index.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+
+<!-- NAVIGATION -->
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Designs by Adrian Buzdug</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav text-center">
+        <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<!-- HEADING -->
+
+ <div class="jumbotron text-center">
+   <h1>Desings by Adrian Buzdug</h1>     
+   <p>Designing / Developing / Perfecting</p>
+ </div>
+
+
+<!-- ABOUT ME -->
+
+<div class="container about">
+  <div class="row">
+    <div class="col-md-6">
+      <img src="assets/facemug.jpg" class="headshot img-circle img-responsive" alt="headshot">
+    </div>
+    <div class="col-md-6">
+      <h2 class="page-header">About Me</h2>
+      <p>Uhm, yeah. Sooo... I don no. Somethin somethin n stuff</p>
+      <p>You want a great website? Huh?! C'mon, don't be shy. Give me your money and I'll build you some shit. Fo reals. If you want a cool design, be ther new cool kid on the block, have them bitches bounce their shit on yo coco then wait no more. Hit the empty account give me all your life savings and all your problems in life will seamlessly diss-a-pear ma niggu'. It's MAGIC.</p>
+      <p><strong>HTML, CSS, Bootstrap, jQuery, some PHP, CodeIgniter</strong></p>
+    </div>
+  </div>
+</div>
+
+
+<!-- PORTFOLIO -->
+
+    <!-- Page Header -->
+<div class="container portheader">
+  <div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header">Portfolio</h2>
+    </div>
+  </div>
+</div>
+
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-6 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x400?text=Coming+Soon" alt="">
+            </a>
+            <h3>
+                <a href="#">Project One</a>
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div class="col-md-6 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x400?text=Coming+Soon" alt="">
+            </a>
+            <h3>
+                <a href="#">Project Two</a>
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+    </div>
+
+    <!-- Projects Row -->
+    <div class="row">
+        <div class="col-md-6 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x400?text=Coming+Soon" alt="">
+            </a>
+            <h3>
+                <a href="#">Project Three</a>
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+        <div class="col-md-6 portfolio-item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/700x400?text=Coming+Soon" alt="">
+            </a>
+            <h3>
+                <a href="#">Project Four</a>
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+    </div>
+
+
+
+</body>
+
+    <!-- jQuery -->
+    <script src="jquery/jquery-3.2.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</html>
